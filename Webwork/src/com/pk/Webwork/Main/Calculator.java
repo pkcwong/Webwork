91441b9f78077a473c3ff77d5d4c6da9625d492f
pkcwong <christopherpk1996@gmail.com>
2016-02-06 14:49:26 +0800

    Build

---
+++ Webwork/src/com/pk/Webwork/Main/Calculator.java
@@ -0,0 +1,33 @@
+package com.pk.Webwork.Main;
+
+public class Calculator {
+
+	org.nfunk.jep.JEP Parser;
+	private String expression;
+
+	public Calculator() {
+
+	}
+
+	/**
+	 * CLEARS ALL DATA
+	 * <p>
+	 */
+	public void clear() {
+		Parser = new org.nfunk.jep.JEP();
+		Parser.addStandardFunctions();
+		Parser.addStandardConstants();
+	}
+	
+	/**
+	 * EVALUATES THE EXPRESSION
+	 * <p>
+	 * 
+	 * @return value of the expression
+	 */
+	public float evaluate() {
+		Parser.parseExpression(this.expression);
+		return (float)(Parser.getValue());
+	}
+
+}
