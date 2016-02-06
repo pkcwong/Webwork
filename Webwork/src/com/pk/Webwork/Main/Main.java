4b5e70813387ec29140849786540b510d52c8e65
pkcwong <christopherpk1996@gmail.com>
2016-02-06 17:37:50 +0800

    Build

--- Webwork/src/com/pk/Webwork/Main/Main.java
+++ Webwork/src/com/pk/Webwork/Main/Main.java
@@ -5,15 +5,18 @@ import java.util.Scanner;
 
 public class Main {
 
-	private static Scanner cin;
+	private static Scanner load;
 	
 	public static void main(String[] args) {
-
+		Calculator cal=new Calculator();
+		cal.input("1*cos(pi)+2*2^2");
+		cal.addVariable("h", 0);
+		System.out.println(cal.evaluate());
 	}
 
 	public static void load(File key) {
 		try {
-			cin = new Scanner(key);
+			load = new Scanner(key);
 		}
 		catch (Exception err) {
 			err.printStackTrace();
